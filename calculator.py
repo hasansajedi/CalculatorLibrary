be74cc475d07a6860c04e7e77aaf10a187b104f9
Hasan <hassansajedi@gmail.com>
2019-10-08 15:09:02 +0330

    Append multiply and division methods and unit tests for them

--- calculator.py
+++ calculator.py
@@ -9,3 +9,14 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
+
+
+def division(first_term, second_term):
+    try:
+        return first_term / second_term
+    except ZeroDivisionError:
+        return None
